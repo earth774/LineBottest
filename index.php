36793ef48d0c2f848a43e1e90ddd5300e89e6848
DESKTOP-85K3NK7\Earth <sutthiphongnuanma@gmail.com>
2018-09-04 23:56:36 +0700

    firth

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
-echo 'Ok!!';
+$host = 'ec2-54-235-160-57.compute-1.amazonaws.com';
+$dbname = 'dbgi88drnrb4b2';
+$user = 'gyyihuipnfrnst';
+$pass = 'bb1d0f4530c5eb1c470b71bfcf9402465a41a8e793220072fb5e30a00daa18ff';
+$connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
+$result = $connection->query("SELECT * FROM polls");
+if ($result !== null) {
+    echo $result->rowCount();
+}
