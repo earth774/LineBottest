241f23bbe87c953093591091f444a7c292981435
DESKTOP-85K3NK7\Earth <sutthiphongnuanma@gmail.com>
2018-09-05 01:00:28 +0700

    firth

--- index_img.php
+++ index_img.php
@@ -8,13 +8,29 @@ $channel_secret = '23305d52c27a46d4ed5c36b63fdc602f';
 $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 if (!is_null($events['events'])) {
-    // Loop through each event
+// Loop through each event
     foreach ($events['events'] as $event) {
-        // Line API send a lot of event type, we interested in message only.
+// Line API send a lot of event type, we interested in message only.
         if ($event['type'] == 'message') {
-            // Get replyToken
             $replyToken = $event['replyToken'];
+            $httpClient = new CurlHTTPClient($channel_token);
+            $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+
             switch ($event['message']['type']) {
+                case 'text':
+                    // Get replyToken
+                    // Reply message
+                    $respMessage = 'Hello, your message is ' . $event['message']['text'];
+                    break;
+                case 'image':
+                    $messageID = $event['message']['id'];
+                    $respMessage = 'Hello, your image ID is ' . $messageID;
+                    break;
+                case 'sticker':
+                    $messageID = $event['message']['packageId'];
+                    // Reply message
+                    $respMessage = 'Hello, your Sticker Package ID is ' . $messageID;
+                    break;
                 case 'video':
                     $messageID = $event['message']['id'];
                     // Create video file on server.
@@ -27,14 +43,14 @@ if (!is_null($events['events'])) {
                     $respMessage = 'Hello, your video ID is ' . $messageID;
                     break;
                 default:
-// Reply message
-                    $respMessage = 'Please send video only';
+                    $respMessage = 'Please send image only';
                     break;
             }
-            $httpClient = new CurlHTTPClient($channel_token);
-            $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-            $textMessageBuilder = new TextMessageBuilder($respMessage);
-            $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+            // $httpClient = new CurlHTTPClient($channel_token);
+            // $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+            // $textMessageBuilder = new TextMessageBuilder($respMessage);
+            // $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+
         }
     }
 }
