6895fdeab75e631be91357115404c93dd6a3bd97
DESKTOP-85K3NK7\Earth <sutthiphongnuanma@gmail.com>
2018-09-05 00:42:26 +0700

    firth

--- index.php
+++ index.php
@@ -20,22 +20,20 @@ if (!is_null($events['events'])) {
                     $replyToken = $event['replyToken'];
                     // Reply message
                     $respMessage = 'Hello, your message is ' . $event['message']['text'];
-                    $httpClient = new CurlHTTPClient($channel_token);
-                    $textMessageBuilder = new TextMessageBuilder($respMessage);
-                    $response = $bot->replyMessage($replyToken, $textMessageBuilder);
                     break;
                 case 'image':
                     $messageID = $event['message']['id'];
                     $respMessage = 'Hello, your image ID is ' . $messageID;
-                    $httpClient = new CurlHTTPClient($channel_token);
-                    $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-                    $textMessageBuilder = new TextMessageBuilder($respMessage);
-                    $response = $bot->replyMessage($replyToken, $textMessageBuilder);
                     break;
                 default:
                     $respMessage = 'Please send image only';
                     break;
             }
+            $httpClient = new CurlHTTPClient($channel_token);
+            $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+            $textMessageBuilder = new TextMessageBuilder($respMessage);
+            $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+
         }
     }
 }
