e3ea2eee76aa1efbfb0bcdb8502dde87f002d5c3
DESKTOP-85K3NK7\Earth <sutthiphongnuanma@gmail.com>
2018-09-05 00:23:14 +0700

    firth

--- index.php
+++ index.php
@@ -1,10 +1,32 @@
 <?php
-$host = 'ec2-54-235-160-57.compute-1.amazonaws.com';
-$dbname = 'dbgi88drnrb4b2';
-$user = 'gyyihuipnfrnst';
-$pass = 'bb1d0f4530c5eb1c470b71bfcf9402465a41a8e793220072fb5e30a00daa18ff';
-$connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
-$result = $connection->query("SELECT * FROM polls");
-if ($result !== null) {
-    echo $result->rowCount();
+require_once './vendor/autoload.php';
+// Namespace
+use \LINE\LINEBot;
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+$channel_token = '0qOVn59vvLLr7lsoCh1lGgcoLf3UdxpdRRyqJbdv/vBZWLTB/weiPZ9VnOO9nssv4XjAMycLv7fCVHM4QIFufSRqxHvasVw2Rry7RLWkIZc8naoH7CfMXXbRtVxCcYkrCKuYC2NflRHcOLEKs8AOmQdB04t89/1O/w1cDnyilFU=';
+$channel_secret = '23305d52c27a46d4ed5c36b63fdc602f';
+// Get message from Line API
+$content = file_get_contents('php://input');
+$events = json_decode($content, true);
+if (!is_null($events['events'])) {
+// Loop through each event
+    foreach ($events['events'] as $event) {
+// Line API send a lot of event type, we interested in message only.
+        if ($event['type'] == 'message') {
+            switch ($event['message']['type']) {
+                case 'text':
+                    // Get replyToken
+                    $replyToken = $event['replyToken'];
+                    // Reply message
+                    $respMessage = 'Hello, your message is ' . $event['message']['text'];
+                    $httpClient = new CurlHTTPClient($channel_token);
+                    $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+                    $textMessageBuilder = new TextMessageBuilder($respMessage);
+                    $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+                    break;
+            }
+        }
+    }
 }
+echo "OK";
