d8f49252ad97d18fc7c55c064a9fcd4cf26bd382
DESKTOP-85K3NK7\Earth <sutthiphongnuanma@gmail.com>
2018-09-05 00:52:58 +0700

    firth

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 require_once './vendor/autoload.php';
 // Namespace
-use \LINE\LINEBot;
-use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 $channel_token = '0qOVn59vvLLr7lsoCh1lGgcoLf3UdxpdRRyqJbdv/vBZWLTB/weiPZ9VnOO9nssv4XjAMycLv7fCVHM4QIFufSRqxHvasVw2Rry7RLWkIZc8naoH7CfMXXbRtVxCcYkrCKuYC2NflRHcOLEKs8AOmQdB04t89/1O/w1cDnyilFU=';
 $channel_secret = '23305d52c27a46d4ed5c36b63fdc602f';
@@ -45,10 +43,10 @@ if (!is_null($events['events'])) {
                     $respMessage = 'Please send image only';
                     break;
             }
-            $httpClient = new CurlHTTPClient($channel_token);
-            $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-            $textMessageBuilder = new TextMessageBuilder($respMessage);
-            $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+            // $httpClient = new CurlHTTPClient($channel_token);
+            // $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+            // $textMessageBuilder = new TextMessageBuilder($respMessage);
+            // $response = $bot->replyMessage($replyToken, $textMessageBuilder);
 
         }
     }
