8369f248ee589673936879df32620365f0bb57aa
DESKTOP-85K3NK7\Earth <sutthiphongnuanma@gmail.com>
2018-09-06 00:18:41 +0700

    send vdo

--- index_sendsticker.php
+++ index_sendsticker.php
@@ -3,8 +3,7 @@ require_once './vendor/autoload.php';
 // Namespace
 use \LINE\LINEBot;
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use \LINE\LINEBot\MessageBuilder\VideoMessageBuilder;
-
+use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
 // Token
 $channel_token = 'WyUPHjT6UfUUuRkCVDtXLWsiQgWzrJ1CJcHumTTDc8GyvMwPYNgVuJjO5htMtKd04XjAMycLv7fCVHM4QIFufSRqxHvasVw2Rry7RLWkIZdYz/qRSRDW8PBixHYOKr5FDxTILJwsLSvcfmqGRzx3KwdB04t89/1O/w1cDnyilFU=';
 $channel_secret = '23305d52c27a46d4ed5c36b63fdc602f';
@@ -16,14 +15,13 @@ if (!is_null($events['events'])) {
     foreach ($events['events'] as $event) {
         // Get replyToken
         $replyToken = $event['replyToken'];
-        // Video
-        $originalContentUrl = 'https://www.select2web.com.com/the-fuji.mp4';
-        $previewImageUrl = 'https://www.select2web.com.com/the-fuji.jpg';
+        // Sticker
+        $packageId = 1;
+        $stickerId = 410;
         $httpClient = new CurlHTTPClient($channel_token);
         $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-        $textMessageBuilder = new VideoMessageBuilder($originalContentUrl, $previewImageUrl);
+        $textMessageBuilder = new StickerMessageBuilder($packageId, $stickerId);
         $response = $bot->replyMessage($replyToken, $textMessageBuilder);
     }
 }
-
 echo "OK";
