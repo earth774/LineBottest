8fd6c04791b7571be3e0d7fcaffdd9274caf669e
DESKTOP-85K3NK7\Earth <sutthiphongnuanma@gmail.com>
2018-09-06 00:30:57 +0700

    send location

--- index_sendvdo.php
+++ index_sendvdo.php
@@ -3,7 +3,7 @@ require_once './vendor/autoload.php';
 // Namespace
 use \LINE\LINEBot;
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use \LINE\LINEBot\MessageBuilder\LocationMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
 // Token
 $channel_token = 'WyUPHjT6UfUUuRkCVDtXLWsiQgWzrJ1CJcHumTTDc8GyvMwPYNgVuJjO5htMtKd04XjAMycLv7fCVHM4QIFufSRqxHvasVw2Rry7RLWkIZdYz/qRSRDW8PBixHYOKr5FDxTILJwsLSvcfmqGRzx3KwdB04t89/1O/w1cDnyilFU=';
 $channel_secret = '23305d52c27a46d4ed5c36b63fdc602f';
@@ -15,14 +15,12 @@ if (!is_null($events['events'])) {
     foreach ($events['events'] as $event) {
         // Get replyToken
         $replyToken = $event['replyToken'];
-        // Location
-        $title = 'I am here';
-        $address = 'Fitness 7 Ratchada';
-        $latitude = '13.7743425';
-        $longitude = '100.5680782';
+        // Image
+        $originalContentUrl = 'https://cdn.shopify.com/s/files/1/1217/6360/products/Shinkansen_Tokaido_ShinFuji_001_1e44e709-ea47-41ac-91e4-89b2b5eb193a_grande.jpg?v=1489641827';
+        $previewImageUrl = 'https://cdn.shopify.com/s/files/1/1217/6360/products/Shinkansen_Tokaido_ShinFuji_001_1e44e709-ea47-41ac-91e4-89b2b5eb193a_grande.jpg?v=1489641827';
         $httpClient = new CurlHTTPClient($channel_token);
         $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-        $textMessageBuilder = new LocationMessageBuilder($title, $address, $latitude, $longitude);
+        $textMessageBuilder = new ImageMessageBuilder($originalContentUrl, $previewImageUrl);
         $response = $bot->replyMessage($replyToken, $textMessageBuilder);
     }
 }
